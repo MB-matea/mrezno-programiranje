dcf1f06fab2b789ac30a95182e33264405a91b66
MB-matea <mateabeslic1@gmail.com>
2021-05-30 02:00:16 +0200

    vjezba9 added

---
+++ vjezba9/ssl_server.py
@@ -0,0 +1,36 @@
+import socket
+from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SHUT_RDWR
+import ssl
+import datetime
+from local_machine_info import print_machine_info
+
+print("Datum:" , datetime.datetime.now())
+
+print_machine_info()
+print("------------------------------------")
+
+listen_addr = '127.0.0.1'
+listen_port = 8082
+server_cert = 'server.crt'
+server_key = 'server.key'
+client_certs = 'client.crt'
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+context.load_verify_locations(cafile=client_certs)
+
+bindsocket = socket.socket()
+bindsocket.bind((listen_addr, listen_port))
+bindsocket.listen(5)
+
+while True:
+    print("Waiting for client")
+    newsocket, fromaddr = bindsocket.accept()
+    print("Client connected: {}:{}".format(fromaddr[0], fromaddr[1]))
+    conn = context.wrap_socket(newsocket, server_side=True)
+    print("SSL established. Peer: {}".format(conn.getpeercert()))
+ 
+    data = conn.recv(4096)
+    print("Received:", data)
+conn.close()
